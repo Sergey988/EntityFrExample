09c8affcfea515fc0efc4768d9239843400cc3fb
Сергей Еременко <sadgramazeka@gmail.com>
2021-11-12 19:54:21 +0200

    Add Task class

---
+++ TaskClass_and_async_await/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TaskClass_and_async_await
+{
+    class Program
+    {
+        //Create a program using the Task class.
+        //User enters some text data, then the program analyzes it and shows the result of: 
+        //count of sentences, words, letters, punctuations and etc.
+
+        //'! ','? ','. '
+        static void CountOfSentencesAsync(string str)
+        {
+            Task.Run(()=> 
+            {
+                int count = 0;
+
+                for (int i = 0; i < str.Length; i++)
+                    if( str[i] == '!' || str[i] == '?' || str[i] == '.' )
+                        count++;    
+      
+                Console.WriteLine($"Count of sentences - {count}");
+            });
+        }
+
+        static void CountOfWordsAsync(string str)
+        {
+            Task.Run(() =>
+            {
+                int count = 0;
+
+                for (int i = 0; i < str.Length; i++)
+                    if (str[i] == ' ')
+                        count++;
+
+
+                Console.WriteLine($"Count of words - {count+1}");
+            });
+        }
+
+        static async Task<int> CountOfLettersAsync(string str)
+        {
+            return await Task.Run(() =>
+            {
+                int count = 0;
+
+                for (int i = 0; i < str.Length; i++)
+                        count++;
+
+                return count;
+            });
+        }
+
+        static async Task<int> CountOfPunctuationsAsync(string str)
+        {
+            return await Task.Run(() =>
+            {
+                int count = 0;
+
+                for (int i = 0; i < str.Length; i++)
+                    if (str[i] == '!' || str[i] == '?' || str[i] == '.' || str[i] == ',' || str[i] == ':' || str[i] == ';')
+                        count++;
+
+                return count;
+            });
+        }
+
+        static async void Run2AsyncMethod(string str)
+        {
+            int res1 = await CountOfLettersAsync(str);
+            int res2 = await CountOfPunctuationsAsync(str);
+
+            Console.WriteLine($"Count of letters - {res1}");
+            Console.WriteLine($"Count of pounctuations - {res2}");
+        }
+
+        static void Main(string[] args)
+        {
+            //Console.Write("Plz enter some text: ");
+            //string str = Console.ReadLine();
+
+            string str = "Lorem; ipsum dolor sit amet, consectetur: adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua? Ut enim ad minim veniam, quis nostrud exercitation: ullamco laboris nisi ut aliquip ex ea, commodo consequat. Duis aute irure dolor in reprehenderit; in, voluptate velit esse cillum dolore, eu fugiat nulla pariatur! Excepteur, sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt, mollit anim id est laborum.";
+
+            CountOfSentencesAsync(str);
+            CountOfWordsAsync(str);
+            Run2AsyncMethod(str);
+
+            Console.WriteLine("End the main thread !!!");
+        }
+    }
+}
---
+++ TaskClass_and_async_await_Ex2/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TaskClass_and_async_await_Ex2
+{
+    class Program
+    {
+        //Create a program to find the next values in some array of numbers: 
+        //the min and max value, average and sum.
+
+        static void GetMinValue(int[] arr)
+        {
+            Task.Run(() =>
+            {
+                int min = arr[0];
+                for (int i = 1; i < arr.Length; i++)
+                    if (arr[i] < min)
+                        min = arr[i];
+
+                Console.WriteLine($"Min value of array - {min}");
+            });
+        }
+
+        static void GetMaxValue(int[] arr)
+        {
+            Task.Run(() =>
+            {
+                int max = arr[0];
+                for (int i = 1; i < arr.Length; i++)
+                    if (arr[i] > max)
+                        max = arr[i];
+
+                Console.WriteLine($"Max value of array - {max}");
+            });
+        }
+
+        static async Task<int> GetAverageValue(int[] arr)
+        {
+            return await Task.Run(() =>
+            {
+                int average = 0;
+                for (int i = 0; i < arr.Length; i++)
+                    average += arr[i];
+                
+                return average/arr.Length;
+            });
+        }
+
+        static async Task<int> GetSumOfArrray(int[] arr)
+        {
+            return await Task.Run(() =>
+            {
+                int sum = 0;
+                for (int i = 0; i < arr.Length; i++)
+                    sum += arr[i];
+                
+                return sum;
+            });
+        }
+
+        static async void Run2AsyncMethod(int[] arr)
+        {
+            int res1 = await GetAverageValue(arr);
+            int res2 = await GetSumOfArrray(arr);
+
+            Console.WriteLine($"Average value of array - {res1}");
+            Console.WriteLine($"Sum of array - {res2}");
+        }
+
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int lengthOfArray = 50;
+            int[] array = new int[lengthOfArray];
+
+            for (int i = 0; i < lengthOfArray; i++)
+            {
+                array[i] = rand.Next(100);
+                Console.Write($"{array[i]} ");
+            }
+            Console.WriteLine();
+
+            GetMinValue(array);
+            GetMaxValue(array);
+            Run2AsyncMethod(array);
+            Console.WriteLine("End the main thread !!!");
+
+            Console.ReadKey();
+        }
+    }
+}
