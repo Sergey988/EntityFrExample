99f0d4ca23c030871a55dc6800b4b62fb08c8302
Сергей Еременко <sadgramazeka@gmail.com>
2021-11-18 11:48:11 +0200

    Add Parallel class tasks

---
+++ Parallel_class_Task_1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace Parallel_class_Task_1
+{
+    //Create a program that writes the multiplication tables into the files.
+    //User inputs the range boundary, then the program write the multiplication table
+    //of each number to separate file.
+    //For example, boundaries: 5, 8
+    //Output:
+
+    //5 * 1 = 5
+    //5 * 2 = 10
+    //...........
+    //5 * 10 = 50
+    //6 * 1 = 6
+    //6 * 2 = 12
+    //...........
+    //8 * 10 = 80
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Plz inputs the range boundary.");
+            //Console.Write("First number: ");
+            //int firstNumber = Int32.Parse(Console.ReadLine());
+            //Console.Write("Second number: ");
+            //int secondNumber = Int32.Parse(Console.ReadLine());
+           
+            int firstNumber = 3;
+            int secondNumber = 10;
+
+            Parallel.For(firstNumber, secondNumber+1, WriteMultiTable);
+
+            static void WriteMultiTable(int number)
+            {
+                string fileName = "MultiTable_" + number + "x.txt";
+
+                Directory.CreateDirectory("Result");
+                string docPath = @"Result\" + fileName;
+
+                //if want add text to the file need to change param to "true"
+                using (StreamWriter sw = new StreamWriter(docPath, false, System.Text.Encoding.Default))
+                {
+
+                    for (int k = 1; k <= 10; k++)
+                        sw.WriteLine($"{number} * {k} = {number * k}");
+
+                    Console.WriteLine($"File {fileName} created: {DateTime.Now.ToString("HH:mm:ss:ms")}");
+
+                }
+            }
+        }
+    }
+}
---
+++ Parallel_class_Task_2/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Parallel_class_Task_2
+{
+
+    //Write a program that read the numbers from the file to the list.
+    //Determine if each number is even, simple or perfect.
+
+
+    class Program
+    {
+
+        static string fileName = "listOfNumbers.txt";
+
+        static void Main(string[] args)
+        {
+            WriteNumbersToFile();
+
+            List<int> myList = GetNumberFromFile();
+            Parallel.ForEach(myList, ShowNumberType);
+
+        }
+
+
+
+        static bool isSimple(int n)
+        {
+            if (n == 0)
+                return false;
+            if (n == 2)
+                return true;
+
+            if (n < 2 || n % 2 == 0)
+                return false;
+
+            for (int i = 3; i <= Math.Sqrt(n); i += 2)
+                if (n % i == 0)
+                    return false;
+
+            return true;
+        }
+        static bool isPerfect(int n)
+        {
+            int sum = 0;
+            if (n == 0)
+                return false;
+
+            for (int i = 1; i < n; i++)
+            {
+                if (n % i == 0)
+                    sum = sum + i;
+
+            }
+            return (sum == n) ? true : false;
+        }
+        static bool isEven(int n)
+        {
+            return (n % 2 == 0) ? true : false;
+        }
+
+        static void ShowNumberType(int number)
+        {
+
+            if (isEven(number) == true)
+                Console.WriteLine($"Number {number} is even");
+            else
+                Console.WriteLine($"Number {number} is odd");
+
+            if (isSimple(number) == true)
+                Console.WriteLine($"Number {number} is simple");
+
+            if (isPerfect(number) == true)
+                Console.WriteLine($"Number {number} is perfect");
+        }
+
+        static List<int> GetNumberFromFile()
+        {
+            string docPath = @"Result\" + fileName;
+            List<int> intList = new List<int>();
+
+            try
+            {
+                using (StreamReader sr = new StreamReader(docPath))
+                {
+                    string c = sr.ReadToEnd();
+                    string[] words = c.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                    
+                    foreach (var item in words)
+                        intList.Add(Int32.Parse(item));
+
+                    //intList = c.Split(' ').Select(Int32.Parse).ToList();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return intList;
+        }
+
+
+        static void WriteNumbersToFile()
+        {
+
+            Directory.CreateDirectory("Result");
+            string docPath = @"Result\" + fileName;
+            Random rnd = new Random();
+
+            using (StreamWriter sw = new StreamWriter(docPath, false, System.Text.Encoding.Default))
+            {
+                int k;
+                for (k = 1; k <= 15; k++)
+                    sw.Write($"{rnd.Next(100)} ");
+
+
+                Console.WriteLine($"File with {k-1} numbers created");
+            }
+
+        }
+    }
+}
